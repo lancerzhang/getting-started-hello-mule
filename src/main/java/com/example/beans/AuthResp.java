fa6fc7d2381ee1daee4cfb483d48078b631b965d
lancerzhang <lancerz@hotmail.com>
2023-08-31 00:04:12 +0800

    testing files

---
+++ src/main/java/com/example/beans/AuthResp.java
@@ -0,0 +1,13 @@
+package com.example.beans;
+
+public class AuthResp {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
